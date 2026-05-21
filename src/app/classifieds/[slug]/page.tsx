6a890283d1d48bc7261eb84c8c9ba1d2f9fe0d24
root <root@srv1489383.hstgr.cloud>
2026-05-21 13:55:31 +0000

    Prevent cached 404 on detail pages

--- src/app/classifieds/[slug]/page.tsx
+++ src/app/classifieds/[slug]/page.tsx
@@ -2,6 +2,9 @@ import { redirect } from "next/navigation";
 import { buildPostMetadata, buildTaskMetadata } from "@/lib/seo";
 import { fetchTaskPostBySlug, fetchTaskPosts } from "@/lib/task-data";
 
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 export const revalidate = 3;
 
 export async function generateStaticParams() {
